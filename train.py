d53d1467d298c6d60795cb0849e32bed52a3ba98
ramanac128 <ramanac@umich.edu>
2017-04-07 14:33:35 -0400

    merged with sam's files, in process of finishing calculations

--- train.py
+++ train.py
@@ -24,10 +24,40 @@ def get_probs(liberal, conservative, total_vocab):
 	for word, freq in conservative.iteritems():
 		conservative[word] = float(freq + 1) / float(total_vocab + len(conservative.keys()))
 
+	return liberal, conservative, total_vocab
 
+def testNaiveBayes(filename, total_vocab, liberal, conservative):
+	with open(filename, 'r') as f:
+		for line in f:
+			username, tweet = line.split(',')
+			conserv_calc = math.log10(0.5)
+			liberal_calc = math.log10(0.5)
 
-	return liberal, conservative
+			# Calculate conservative and liberal calculations for user via naive bayes
+			for word in tweet:
+				if word in conservative:
+					conserv_calc += math.log10(float(conservative[word]))
+					
+				else:
+					conserv_calc += math.log10(1.00 / float(len(conservative.keys()) + total_vocab))
+					
+				if word in liberal:
+					liberal_calc += math.log10(float(liberal[word]))
+				else:
+					liberal_calc += math.log10(1.00 / float(len(liberal.keys()) + total_vocab))
 
+			# Add in who the user follows 
+			# *** SAM ADD IN THE LIST OF CONSERVATIVE/LIBERAL USERS HERE **** 
+			# liberal_users = []
+			# conserv_users = []
+
+			# Determine whether document is more of a lie or truth
+			if conserv_calc > liberal_calc:
+				#print filename, 'lie'
+				return 'conservative'
+			else:
+				#print filename, 'truth'
+				return 'liberal'
 
 
 def main():
@@ -39,9 +69,19 @@ def main():
 	liberal = get_freqs(lib, liberal)
 	conservative = get_freqs(con, conservative)
 
-	total_vocab = len(liberal.keys()) + len(conservative.keys())
+	# Get distinct words
+	vocab = []
+	for word in liberal:
+		if word not in vocab:
+			vocab.append(word)
 
-	liberal, conservative = get_probs(liberal, conservative, total_vocab)
+	for word in conservative:
+		if word not in vocab:
+			vocab.append(word)
+
+	total_vocab = len(vocab)
+
+	liberal, conservative, total_vocab = get_probs(liberal, conservative, total_vocab)
 
 	print liberal
 	print "=================================="
