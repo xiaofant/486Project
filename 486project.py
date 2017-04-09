979e23f6bf7359eead00b8b46211e399871511be
xiaofant <xiaofant@umich.edu>
2017-04-09 12:19:32 -0400

    Include main and preprocess function

---
+++ 486project.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import preprocessor as p
+import sys
+import re
+
+def isDate(s):
+    if re.match(r'\w+/\d{2}/\d{4}|\w+-\d{2}-\d{4}', s):
+        return True
+    return False
+
+def preprocess(tweet):
+    p.set_options(p.OPT.URL, p.OPT.EMOJI)
+    clean = p.clean(tweet)
+    clean = clean.replace("#","")
+
+    # copy of tokenizer from my previous project
+    # using http://grammar.about.com/od/words/a/EnglishContractions.html for reference of contractions
+    cont = open("486Project/Contractions in English")
+    line = cont.readline()
+    contractions = {}
+
+    while line:
+        line = line.strip()
+        line = re.split('\s+', line)
+        contraction = line[0]
+        expand = ""
+        for i in range(1, len(line)):
+            expand += line[i] + " "
+        contractions[contraction] = expand[:-1]
+        line = cont.readline()
+
+    input = clean.lower()
+    input = input.strip()
+    input = re.split('\s+', input)
+
+    output = []
+
+    for i in range(0, len(input)):
+        word = input[i]
+        # remove all other symbols/characters that are not mentioned
+        word = re.sub("[^a-z0-9-.'/,]", '', word).strip()
+        if word in contractions:
+            match = contractions[word]
+            output.extend(re.split('\s+', match))
+        elif len(word) > 2 and (word[-2:] == "'s" or word[-2:] == "s'"):
+            # handle possessive
+            output.append(word[:-2])
+            output.append("'s")
+        elif isDate(word):
+            output.append(word.strip())
+        elif re.match(r'\w*,\w*', word):
+            # tokenization of ,
+            if re.match('\d+,\d+', word):
+                # case of number
+                output.append(word)
+            else:
+                word = re.sub(',', '', word).strip()
+                if word != "":
+                    output.append(word)
+        elif re.match(r'\w*\.\w*', word):
+            # tokenization of .
+            if word == ".":
+                continue
+            elif re.match(r'\w{2,}\.', word) and word != "mr." and word != "dr." and word != "st." \
+                    and word != "oz." and word != "ms." and word != "jr." and word != "sr." and \
+                            word != "mt." and word != "no." and word != "sq.":
+                # some popular two-characters abbreviations from http://www.englishleap.com/other-resources/abbreviations
+                output.append(word[:-1])
+            else:
+                # take else and all one char ended with a '.' as abbreviations/acronyms
+                output.append(word.strip())
+        else:
+            word = re.sub("[^a-z0-9-.]", ' ', word).strip()
+            if word == '':
+                continue
+            output.append(word)
+
+    return output
+
+
+if __name__ == '__main__':
+    # files = os.listdir(sys.argv[1])
+    state = sys.argv[1]
+    file = open("486Project/tweets_"+state+".txt",'r')
+    users = {}
+    for line in file:
+        username, tweet = line.split(',',1)
+        tweet = preprocess(tweet)
+
+        # testNaiveBayes: take preprocessed tokens of 1 tweet,
+        # return the result of classification
+
+        # take result == conservative as 1, liberal as -1
+        if username in users:
+            users[username] = users[username] + result
+
+    NumConservative = 0
+    NumLiberal = 0
+    for user in users:
+        if users[user] > 0:
+            NumConservative += 1
+        elif users[user] < 0:
+            NumLiberal += 1
+
+    ConservativePercentage = NumConservative/(NumLiberal+NumConservative)
+    LiberalPercentage = NumLiberal/(NumLiberal+NumConservative)
+
+    print ("Conservative: "+str(ConservativePercentage))
+    print ("Liberal: "+str(LiberalPercentage))
